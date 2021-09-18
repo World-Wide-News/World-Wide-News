e48777cdf690a351c907e4e0a5caae097424118b
Kasthuri Menon <kasthuri.menon1@gmail.com>
2021-09-18 17:13:54 -0400

    Kasthuri: Initial commit

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,5 +1,6 @@
-// const axios = require('axios');
-// const path = require('path');
+const axios = require('axios');
+const express = require('express');
+const path = require('path');
 // const fs = require('fs');
 // const bcrypt = require('bcryptjs');
 // const models = require('../models/mtaModels');
@@ -28,7 +29,58 @@
 
 // // };
 
-// const apiController = {};
+const apiController = {};
+apiController.getDetails = async (req, res, next) => {
+  const countryName = 'australia';
+  // add the request details for the fetch request that will get the news data
+  const requestDetails = {
+    method: 'GET',
+    url: 'https://free-news.p.rapidapi.com/v1/search',
+    params: {
+      q: countryName, lang: 'en', page: '1', page_size: '5',
+    },
+    headers: {
+      'x-rapidapi-key': 'c9dd5fae0bmshb0c6910ac9ff173p1739a1jsn7a43e27d0bc4',
+      'x-rapidapi-host': 'free-news.p.rapidapi.com',
+    },
+  };
+
+  axios.request(requestDetails)
+    .then((response) => {
+      //   console.log(response.data.articles);
+      //   res.locals.articles = response.body;
+      const newObj = {};
+      // iterate through the articles recieved and save the required fields in a new object
+      for (let i = 0; i < response.data.articles.length; i += 1) {
+        const currentItem = response.data.articles[i];
+        newObj[i] = {
+          title: currentItem.title,
+          summary: currentItem.summary,
+          link: currentItem.link,
+          media: currentItem.media,
+        };
+      }
+      // assign it to res.locals and send back
+      res.locals.articles = newObj;
+
+      return next();
+    }).catch((err) => {
+      const defaultErr = {
+        log: 'Error handler caught an error inside getProducts',
+        status: 500,
+        message: { err: `An error occurred inside a middleware named getProducts : ${err}` },
+      };
+      next(defaultErr);
+    });
+
+  // get the country name of the country clicked by the user on the FE and store it in a variable
+
+  // Send a server side request to the API
+  // search the API with COUNTRY and SAVE THE RESPONSE
+  // using axios, fetch the response and save the required details like Title, Summary, url link
+  // call next() and send it to the next middleware
+  // send it back to front end as required by the FE
+};
 
 // apiController.getData = async (req, res, next) => {
 //   // const mtaOut = [];
@@ -187,4 +239,4 @@
 //   }
 // };
 
-// module.exports = apiController;
+module.exports = apiController;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,15 @@
-// const express = require('express');
-// const apiController = require('../controllers/apiController');
+const express = require('express');
+const apiController = require('../controllers/apiController');
 
-// const router = express.Router();
+const router = express.Router();
 
 // // router.get('/', apiController.getData, (req, res) => {
 // //   res.send('yes');
 // // });
+router.get('/getDetails', apiController.getDetails, (req, res) => {
+  console.log('we are in the last middleware func');
+  return res.status(200).send(res.locals.articles);
+});
 
 // router.get('/map', apiController.createMap, (req, res) => {
 //   res.send(res.locals.data).status(200);
