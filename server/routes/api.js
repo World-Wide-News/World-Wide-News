51618fa7ea93042899bd8fa1d467cbe695ee7d3f
Kasthuri Menon <kasthuri.menon1@gmail.com>
2021-09-20 13:01:14 -0400

    api file changes copied from olg login branch to new login branch

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,6 +7,27 @@ router.get('/population/:countryName', apiController.getPopulationData, (req, re
 
 router.get('/getArticles/:countryName', apiController.getArticles, (req, res) => res.status(200).json(res.locals.articles));
 
+// route to sign-up
+router.post('/signup', apiController.createUser,
+  (req, res) => {
+    res.status(200).send(res.locals.user);
+  });
+
+// route and middlewares to execute when user tries to login
+router.post('/login',
+  apiController.verifyUser,
+  apiController.getUserData,
+  (req, res) => {
+    res.status(200).json(res.locals.data);
+  });
+
+// route and middlewares to execute when user adds favourite links
+router.post('/addFav/?:title',
+  apiController.addFav,
+  (req, res) => {
+    res.status(200).send(res.locals.user);
+  });
+
 // router.get('/map', apiController.createMap, (req, res) => {
 //   res.send(res.locals.data).status(200);
 // });
